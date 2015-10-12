323b0aa6db5abd96104cb276fcde9b5bccebc583
Ollie Coulson <olliecoulson@Ollies-MacBook-Pro.local>
2015-10-12 18:43:16 +0100

    18.43 12/10/15: Added code for biggerPaper method, it is wrong and doesn't use recursive. Need to get it to call itself on an earlier recursion.

--- PaperSize.groovy
+++ PaperSize.groovy
@@ -60,26 +60,39 @@ String trimmed = input.substring(1, input.length());
 
 PSize newSize = new PSize();
 newSize = newPaper(trimmed, a0);
+newSize.setArea();
+int newArea = newSize.getArea();
 
-PSize newPaper(String trimmed, PSize a) {
-	PSize temp = new PSize();
-	temp.setWidth(0);
-	temp.setLength(0);
-	temp.setArea();
-	
+PSize newPaper(String trimmed, PSize basic) {
 	int value = Integer.parseInt(trimmed);
+	int length = trimmed.length();
 	if (value == 0) {
-		if (trimmed.length() == 1) {
-			return a;
-		}
-		else {
-			
-		}
+		PSize temp = new PSize();
+		temp = biggerPaper(length, basic); 
 	}
 	else {
-		//insert code for smaller than A0
+		//call smaller method
 	}
 }
 
+PSize biggerPaper(int length, PSize basic) {
+	if (length == 1) {
+		return basic;
+	}
+	else {
+		PSize temp = new PSize();
+		temp.setWidth(biggerPaper().getWidth);
+		temp.setLength(biggerPaper().getLength);
+		
+		if (temp.getWidth() < temp.getLength())	{
+			temp.setWidth(temp.getWidth() * 2);
+			return temp;
+		}		
+		else {
+			temp.setLength(temp.getLength() *2)
+			return temp;
+		}		
+	}
+}
 
-System.out.println();
+System.out.println(newArea);
